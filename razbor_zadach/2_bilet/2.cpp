122c6246386beefcc11e1937b29e145051c5b795
LegoOlegGelo <leviter517@gmail.com>
2022-01-11 15:22:44 +0300

    2 bilet 2.cpp upgraded

--- razbor_zadach/2_bilet/2.cpp
+++ razbor_zadach/2_bilet/2.cpp
@@ -22,18 +22,18 @@ float get_perimeter(point a, point b, point c)
     return get_len(a, b) + get_len(a, c) + get_len(b, c);
 }
 
-int main()
+point* read_data(int& count)
 {
     // открытие, проверка данных
     ifstream fin("points.txt");
-    
+
     if (!fin.is_open())
     {
         cout << "File open error" << endl;
 
-        return -1;
+        return nullptr;
     }
-    
+
     int N = 0;
 
     fin.seekg(0, ios_base::end);
@@ -62,7 +62,7 @@ int main()
     if (N % 2 == 1)
     {
         cout << "File contains invalid data" << endl;
-        return -2;
+        return nullptr;
     }
 
     N /= 2;
@@ -70,7 +70,7 @@ int main()
     if (N < 3)
     {
         cout << "File contains less than 3 points" << endl;
-        return -3;
+        return nullptr;
     }
 
 
@@ -101,16 +101,26 @@ int main()
         else
             points[i++ / 2].Y = a;
     }
-    
 
-    // вывод
+    fin.close();
+
+    count = N;
+    return points;
+}
+
+int main()
+{
+    // чтение данных
+    int N;
+    point* points = read_data(N);
+
+    // вывод входных данных
     for (size_t i = 0; i < N; i++)
     {
         point p = points[i];
         cout << p.X << " " << p.Y << endl;
     }
     cout << endl;
-
 
     // перебор
     float max_perimeter = 0;
@@ -130,7 +140,6 @@ int main()
                 }
             }
 
-
     // вывод результатов
     cout << "3 points with max perimeter = " << max_perimeter << endl;
 
@@ -140,7 +149,6 @@ int main()
 
         cout << p.X << " " << p.Y << endl;
     }
-
 
     // очищение памяти, выход
     delete[] points;
